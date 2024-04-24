f2df54640a35632c1687fd65486414fae0d5569c
Quezty <storlijoachim@gmail.com>
2024-04-24 17:25:34 +0200

    I have commitment issues

--- spill/level.py
+++ spill/level.py
@@ -1,51 +1,84 @@
+import sys
 import pygame
-from tiles import Tile
+from support import import_csv_layout, import_cut_graphic
 from settings import tile_size, screen_width
+from tiles import StaticTile
+from decoration import Sky
 from player import Player
 
 class Level:
     def __init__(self, level_data, surface):
-
-        # level setup
+        # general setup
         self.display_surface = surface
-        self.setup_level(level_data)
         self.world_shift = 0
 
-    def setup_level(self, layout):
-        self.tiles = pygame.sprite.Group()
+        # player setup
+        player_layout = import_csv_layout(level_data['player'])
         self.player = pygame.sprite.GroupSingle()
+        self.goal = pygame.sprite.GroupSingle()
+        self.player_setup(player_layout)
+        
+
+        # terrain setup
+        terrain_layout = import_csv_layout(level_data['terrain'])
+        self.terrain_sprites = self.create_tile_group(terrain_layout, 'terrain')
+
+        # decoration
+        self.sky = Sky()
+
+    def create_tile_group(self, layout, type):
+        sprite_group = pygame.sprite.Group()
+
         for row_index, row in enumerate(layout):
-            for col_index, cell in enumerate(row): 
+            for col_index, val in enumerate(row):
+                if val != '-1':
+                    x = col_index * tile_size
+                    y = row_index * tile_size
+
+                    if type == 'terrain':
+                        terrain_tile_list = import_cut_graphic('maps_and_images\\terrain_tiles.png')
+                        tile_surface = terrain_tile_list[int(val)]
+                        sprite = StaticTile(tile_size, x, y, tile_surface)
+                        sprite_group.add(sprite)
+
+                    sprite_group.add(sprite) 
+
+        
+        return sprite_group
+
+    def player_setup(self, layout):
+        for row_index, row in enumerate(layout):
+            for col_index, val in enumerate(row):
                 x = col_index * tile_size
                 y = row_index * tile_size
-
-                if cell == 'X':
-                    tile = Tile((x, y), tile_size)
-                    self.tiles.add(tile)
-                if cell == 'P':
-                    player_sprite = Player((x, y))
-                    self.player.add(player_sprite)
+                if val == '1':
+                    sprite = Player((x, y))
+                    self.player.add(sprite)
+                if val == '0':
+                    coin_surface = pygame.image.load("maps_and_images\\Coin.png")
+                    sprite = StaticTile(tile_size, x, y, coin_surface)
+                    self.goal.add(sprite)     
 
     def scroll_x(self):
         player = self.player.sprite
         player_x = player.rect.centerx
         direction_x = player.direction.x
 
-        if player_x < screen_width / 4 and direction_x < 0:
+        if player_x < screen_width / 6 and direction_x < 0:
             self.world_shift = 8
             player.speed = 0
-        elif player_x > screen_width - (screen_width / 4) and direction_x > 0:
+        elif player_x > screen_width - (screen_width / 6) and direction_x > 0:
             self.world_shift = -8
             player.speed = 0
         else:
             self.world_shift = 0
             player.speed = 8
-        
+
     def horizontal_movement_collision (self):
         player = self.player.sprite
         player.rect.x += player.direction.x * player.speed
 
-        for sprite in self.tiles.sprites():
+        for sprite in self.terrain_sprites.sprites():
             if sprite.rect.colliderect(player.rect):
                 if player.direction.x < 0:
                     player.rect.left = sprite.rect.right
@@ -56,7 +89,7 @@ class Level:
         player = self.player.sprite
         player.apply_gravity()
 
-        for sprite in self.tiles.sprites():
+        for sprite in self.terrain_sprites.sprites():
             if sprite.rect.colliderect(player.rect):
                 if player.direction.y > 0:
                     player.rect.bottom = sprite.rect.top
@@ -65,15 +98,31 @@ class Level:
                     player.rect.top = sprite.rect.bottom
                     player.direction.y = 0
 
+    def win_check(self):
+        win_condition = False
+        player = self.player.sprite
+        for sprite in self.goal.sprites():
+            if sprite.rect.colliderect(player.rect):
+                print('You found the sacred coin. Congratulations!')
+                pygame.quit()
+                sys.exit()
+            else:
+                pass
 
     def run(self):
-        # Level tiles
-        self.tiles.update(self.world_shift)
-        self.tiles.draw(self.display_surface)
-        self.scroll_x()
-        
-        # player
+        # decoration
+        self.sky.draw(self.display_surface)
+
+        # run the entire game / level
+        self.terrain_sprites.draw(self.display_surface)
+        self.terrain_sprites.update(self.world_shift)
+
+        # Player
         self.player.update()
+        self.scroll_x()
         self.horizontal_movement_collision()
         self.vertical_movement_collision()
         self.player.draw(self.display_surface)
+        self.goal.update(self.world_shift)
+        self.goal.draw(self.display_surface)
+        self.win_check()
