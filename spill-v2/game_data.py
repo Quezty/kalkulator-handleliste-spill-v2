a99ae7162e6fc168c698ea622d18a426a93a2975
Quezty <storlijoachim@gmail.com>
2024-04-29 14:15:59 +0200

    freedom, peace

--- spill-v2/game_data.py
+++ spill-v2/game_data.py
@@ -1,4 +1,4 @@
 level_0 = {
-           'terrain' : "maps_and_images/redone_map_terrain.csv",  
-           'player' : "maps_and_images/redone_map_Player.csv" 
+           'terrain' : "maps_and_images\\redone_map_terrain.csv",  
+           'player' : "maps_and_images\\redone_map_Player.csv" 
            }
